017626ef7d428a9695cb89a65c75b56b9d4bdc5e
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-09-23 19:38:52 +0200

    Added unit tests to test PhotoFilter class

--- test-app/src/js/photoHandler.js
+++ test-app/src/js/photoHandler.js
@@ -2,6 +2,8 @@ import { PhotoFilter } from '../../../lib/photoAssistant/photoFilter.js'
 
 /**
  * Class that handles filter method, value and adds clicked images to an array.
+ *
+ * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
  */
 export class PhotoHandler {
   #clickedImages = []
@@ -9,7 +11,7 @@ export class PhotoHandler {
   /**
    * Entry point method.
    *
-   * @param {*} clickedImage
+   * @param {HTMLImageElement} clickedImage - most recent image that has been clicked.
    */
   runPhotoHandler (clickedImage) {
     this.#addClickedImagesToArray(clickedImage)
@@ -18,23 +20,27 @@ export class PhotoHandler {
   /**
    * Method to add filter to image in the DOM.
    *
-   * @param {*} filterMethod - method to filter image.
-   * @param {*} filterValue - value of which to filter image.
+   * @param {string} filterMethod - method to filter image.
+   * @param {string} filterValue - value of which to filter image.
    */
   addFilter (filterMethod, filterValue) {
     const photoFilter = new PhotoFilter(filterMethod, filterValue)
 
+    // Move this logic to the PhotoFilter class of the lib.
     this.#clickedImages.forEach((image) => {
       photoFilter.runPhotoFilter(image)
     })
+
+    console.log(this.#clickedImages)
   }
 
   /**
    * Method to add clicked images to an image array.
    *
-   * @param {*} clickedImage - recently clicked image to be added to clicked images array.
+   * @param {HTMLImageElement} clickedImage - recently clicked image to be added to clicked images array.
    */
   #addClickedImagesToArray (clickedImage) {
+    // Also move this logic to the lib as a util.
     if (this.#clickedImages.length === 0) {
       this.#clickedImages.push(clickedImage)
     } else {
---
+++ test/photoFilter.test.js
@@ -0,0 +1,34 @@
+import { PhotoFilter } from '../lib/photoAssistant/photoFilter.js'
+
+test('Tests filter method input of incorrect type', () => {
+  const filterMethod = 48
+  const filterValue = '80%'
+
+  expect(() => new PhotoFilter(filterMethod, filterValue)).toThrow('Invalid input')
+})
+
+test('Tests filter value input of incorrect type', () => {
+  const filterMethod = 'contrast'
+  const filterValue = false
+
+  expect(() => new PhotoFilter(filterMethod, filterValue)).toThrow('Invalid input')
+})
+
+test('Tests filter method and filter value input of correct type', () => {
+  const filterMethod = 'contrast'
+  const filterValue = '80%'
+
+  const photoFilter = new PhotoFilter(filterMethod, filterValue)
+
+  expect(photoFilter).toBeDefined()
+})
+
+test('Tests filtering an invalid image', () => {
+  const image = null
+  const filterMethod = 'saturation'
+  const filterValue = '50%'
+
+  const photoFilter = new PhotoFilter(filterMethod, filterValue)
+
+  expect(() => photoFilter.runPhotoFilter(image)).toThrow('Invalid image')
+})

fb38378e77dcfb404a74df80c653cd96dd35c815
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-09-25 12:35:08 +0200

    Start of photoGallery class by adding some logic for calc of grid layout

--- test-app/src/js/index.js
+++ test-app/src/js/index.js
@@ -10,28 +10,13 @@ for (let i = 0; i < images.length; i++) {
     console.log('Image has been clicked')
 
     photoAssistant.addImage(images[i])
-
-    // Added for testing.
-    const filterMethod = 'Contrast'
-    const filterValue = '150%'
-
-    photoAssistant.filterPhotos(filterMethod, filterValue)
-
-    // Testing.
-    // const photoFilter = new PhotoFilter(filterMethod, filterValue)
-    // photoFilter.runPhotoFilter('https://upcdn.io/W142hJk/raw/demo/4kK8CPmH6V.jpg')
-
-    // photoAssistant.filterPhotos(filterMethod, filterValue)
-
-    // Testing.
-    // photoHandler.saveImageToStorage(images[i])
-    // photoHandler.getImageFromStorage(images[i])
   }
   )
 }
 
-/* const galleryForm = document.getElementById('gallery')
-galleryForm.addEventListener('submit', (event) => {
-  event.preventDefault()
-  photoHandler.displayInGallery()
-}) */
+// Added for testing.
+// Add another event listener that listens to form submit event.
+// const filterMethod = 'Contrast'
+// const filterValue = '150%'
+
+// photoAssistant.filterPhotos(filterMethod, filterValue)
